133cc7ca91fa90c7f4d180f2ac0ed0dc5df43bb0
Abdullah al Thalha <abdullahalthalha@gmail.com>
2023-07-09 11:22:47 +0600

    TextFormField added

--- lib/main.dart
+++ lib/main.dart
@@ -18,24 +18,106 @@ class MyApp  extends StatelessWidget {
   }
 }
 class Homepage extends StatelessWidget {
-  const Homepage({Key? key}) : super(key: key);
+   Homepage({Key? key}) : super(key: key);
+  TextEditingController emailController=TextEditingController();
+  TextEditingController passwordController=TextEditingController();
+   TextEditingController numberController=TextEditingController();
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(///Scaffold :works on individual screen
+    return Scaffold(
+
+      ///Scaffold :works on individual screen
       appBar: AppBar(
         title: Text('Home Screen'),
         backgroundColor: Colors.green,
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.add_call)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.camera)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.add_call)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.camera)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
         ],
 
       ),
 
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+              mainAxisAlignment:MainAxisAlignment.center,
+              children:[
+                TextFormField(
+                   validator:(String?value){
+                     if(value!.isEmpty){
+                       return 'enter email';
+    }
+                     else{
+                       return null;
+                     }
+                     },///for mandotory email and password validator is being used and in number is not being used;
+                  controller:emailController,
+                  ///obscureText: true,
+                  decoration: InputDecoration(
+                  hintText: 'email',
+                  fillColor:Colors.grey.shade500,
+                  filled: true,
+
+                  ),
+                ),
+
+
+                  SizedBox(height: 20,),
+                  TextFormField(
+                  controller:passwordController,///its mendatory to set controller,validator for every textformfield;
+
+                  decoration: InputDecoration(
+                  hintText: 'password',
+                  fillColor:Colors.grey.shade500,
+                  filled: true,
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.only(topLeft:Radius.circular(10), topRight:Radius.circular(10) ),
+
+                  )
+                  ),
+
+
+                  ),
+
+
+
+                SizedBox(height: 20,),
+                TextFormField(
+                  controller:numberController,///its mendatory to set controller,validator for every textformfield;
+
+                  decoration: InputDecoration(
+                      hintText: 'number',
+                      fillColor:Colors.grey.shade500,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.only(topLeft:Radius.circular(10), topRight:Radius.circular(10) ),
+
+                      )
+                  ),
+
+
+                )
+
+
+
+              ]
+
+          ),
+
+        ),
+      ),
+
+
+
     );
   }
+
+
 }
 
 
